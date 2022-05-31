31d5a176a16025fda952c3aa467bca4baa5dc00e
fire-lit-band <fire-lit-band@outlook.com>
2022-05-31 17:59:36 +0800

    renew

--- read_todo.py
+++ read_todo.py
@@ -1,1 +1,1 @@
-import jsonimport pandas as pddef read_todo():    todo=pd.read_csv("todo.csv")    return todoif __name__=="__main__":    print(read_todo())
+import jsonimport pandas as pddef read_todo():    todo=pd.read_csv("todo.csv")    return tododef display_todo():    todo = pd.read_csv("todo.csv")    return todo["name"]if __name__=="__main__":    print(display_todo())
--- to_todo.py
+++ to_todo.py
@@ -1,11 +1,22 @@
-import json
+
 
 import pandas as pd
+
 def to_todo(todo):
     data=pd.DataFrame(todo)
     data.to_csv("todo.csv")
+
+def add_todo(todo):
+    column_name=todo.columns
+    new_task={}
+    for i in column_name:
+        content=input(i+":")
+        new_task[i]=content
+    todo=todo.append(pd.Series(new_task),ignore_index=True)
+    return todo
+
 if __name__=="__main__":
-    with open("todo.json",'r') as file:
-         todo=json.load(file)
-    to_todo(todo)
-    print(todo)
+    todo=pd.read_csv("todo.csv")
+    new_todo=add_todo(todo)
+    new_todo.to_csv("todo.csv",index=False)
+    print(new_todo)
