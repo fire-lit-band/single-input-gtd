0840d5d10095a7ac0fbf9684aa5b089cba1c7411
fire-lit-band <fire-lit-band@outlook.com>
2022-07-05 16:33:09 +0800

    renew

--- read_todo.py
+++ read_todo.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import time
 from datetime import datetime
+import to_todo
 
 
 def read_all_content(file="todo.csv"):
@@ -11,7 +12,9 @@ def read_all_content(file="todo.csv"):
 
 def today_inbox_todo(file="todo.csv"):
     todo = pd.read_csv(file)
-    return todo[todo.father.isnull() & todo.start_time.isnull()].name
+    todo=todo[todo.father.isnull() & todo.start_time.isnull()]
+    for i in todo.index:
+        print_child(i,0,todo)
 
 
 def display_all_task(file="todo.csv"):
@@ -22,7 +25,7 @@ def today_ddl(file="todo.csv"):
     todo=pd.read_csv(file)
     ddl=todo[todo.father.isnull() & (~todo.start_time.isnull())]
     for i in ddl.index:
-        print(i,ddl.at[i,'name'],remain_time(ddl.at[i,'start_time']))
+        print_child(i,0,todo)
 
 def remain_time(ddl):
     remain_time_stamp=ddl-time.time()
@@ -37,8 +40,20 @@ def remain_time(ddl):
             return "还剩下"+str(remain_tuple[3])+"小时,"+str(remain_tuple[4])+"分钟"
 
 def display_todo():
-    print(today_inbox_todo())
+    today_inbox_todo()
     today_ddl()
+
+
+def print_child(i,incident,todo):
+    leaf=eval(todo.at[i,'leaf'])
+    if not np.isnan(todo.loc[i].start_time):
+        print(" " * incident + str(i) + " " + todo.at[i, 'name']+" "+remain_time(todo.at[i,'start_time']))
+    else:
+        print(" " * incident + str(i) + " " + todo.at[i, 'name'] )
+    if not leaf==[]:
+        for j in leaf:
+            print_child(to_todo.find('id',j)[0],incident+2,todo)
+
 
 if __name__ == "__main__":
     today_ddl()
