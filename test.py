c990873d43ef4f6bae24336fa87280c9d1c43aaa
PRAGM <fire-lit-band@outlook.com>
2022-08-19 16:23:07 +0800

    bug fix

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtGui,QtCore
 from PyQt5.QtGui import QIcon, QBrush, QColor
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtGui import QStandardItem, QStandardItemModel,QKeySequence
 from PyQt5.QtWidgets import QMainWindow, QTreeView, QApplication
 from PyQt5 import QtWidgets
 import pandas as pd
 import numpy as np
+from function import clockUI
 
 class MainWindow(QMainWindow):
-    class DemoTreeView():
+    class DemoTreeView(QWidget):
         class Item(QStandardItem):
             def __init__(self, text,key):
                 super().__init__(text)
@@ -18,7 +19,7 @@ class MainWindow(QMainWindow):
                 self.key=key
 
             def clone(self) -> 'DemoTreeView.Item':
-                return DemoTreeView.Item(self.original_text)
+                return MainWindow.DemoTreeView.Item(self.original_text)
 
             def say(self):
                 print(self.original_text)
@@ -69,13 +70,12 @@ class MainWindow(QMainWindow):
 
 
         def __init__(self, parent=None):
-            super(DemoTreeView, self).__init__(parent)
+            super().__init__(parent)
             self.initUi()
 
         def on_model_data_changed_handle(self, index):
             print("emit")
-            data=self.model.i
-            nvisibleRootItem().child(0,0).data(Qt.UserRole)
+            data=self.model.invisibleRootItem().child(0,0).data(Qt.UserRole)
             print(data)
         def initUi(self):
             self.model = QStandardItemModel(self)
@@ -96,6 +96,7 @@ class MainWindow(QMainWindow):
                         child = self.Item(df.at[i, 'name'],df.at[i,'name'])
                         #child.setText(0, df.at[i, 'name'])
                         j['self'].appendRow([child])
+                        alldata.append({'id': df.at[i, 'id'], 'self': child})
                         break
 
             self.model.dataChanged.connect(self.on_model_data_changed_handle)
@@ -105,26 +106,37 @@ class MainWindow(QMainWindow):
             self.treeView.selectionModel().selectionChanged.connect(self.on_view_selectionChanged)
 
             self.treeView.expandAll()
-            self.setCentralWidget(self.treeView)
+
 
 
         def on_view_selectionChanged(self, selection: 'QItemSelection'):
+            if len(selection.indexes())==0:
+                return
             row = selection.indexes()[0].row()
             item = self.model.itemFromIndex(selection.indexes()[0])
             item.say()
 
     def __init__(self):
         super().__init__()
+        self.setCentralWidget(QWidget(self))
+
         self.Init_UI()
     def Init_UI(self,parent=None):
-        Newtree=MainWindow.DemoTreeView()
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        Newtree=MainWindow.DemoTreeView(self)
+        QShortcut(QKeySequence(" "), self).activated.connect(self.clock)
+        self.lineEdit = QtWidgets.QLineEdit(self)
         self.lineEdit.setGeometry(QtCore.QRect(40, 30, 511, 31))
         self.lineEdit.setObjectName("lineEdit")
         v_box=QVBoxLayout()
-        v_box.addWidget(Newtree)
+        v_box.addWidget(Newtree.treeView)
         v_box.addWidget(self.lineEdit)
-        self.setLayout(v_box)
+        self.centralWidget().setLayout(v_box)
+
+
+    def clock(self):
+        item = self.model().itemFromIndex(index)
+        print(item.original_text)
+        clockUI.main(item.original_text)
 
 
 
--- user_UI.py
+++ user_UI.py
@@ -4,7 +4,7 @@ import pandas as pd
 import function.calendarUItest as cl
 import function.clockUI as clock
 import time
-from datetime import date,datetime
+from datetime import date,datetime,timedelta
 from classes.Finished import Finished
 from classes.Task import Task
 from pathlib import Path
@@ -32,15 +32,12 @@ def add_record(Record: Finished,file="record_sample.csv"):
     column_names = pd.concat([column_names, pd.Series(new_record).to_frame().T], ignore_index=True)
     column_names.to_csv(file_name, index=False)
 
-def set_default(key,values,datetuple,content,havecontent):
-    if not havecontent:
-        if key==[]:
-            key=[""]
-        values['father']=key[0]
-        values['id'] = time.time()
-    else:
-        content.update(values)
-        values=content
+def set_default(key,values,datetuple,content): # 设置日期的时候的一些默认选项
+    if key==[]:
+        key=[""]
+    values['id'] = time.time()
+    content.update(values)
+    values=content
     if datetuple == None:
 
         if values['hour'].isdigit():
@@ -95,22 +92,45 @@ def set_default(key,values,datetuple,content,havecontent):
 
     del values['hour'],values['minute']
     return values
-
-def todo_content(key,todoTree,df,file,content=None):
-    if content==None:
-        content={'name':'','num':'','hour':'','minute':'','repetition_gap':1,'task_value':''}
-        datetuple = None
-        havecontent=False
+def revise_content(key,df,file,content):
+    if not np.isnan(content['start_time']):
+        struct_times = time.localtime(content['start_time'])
+        print(struct_times)
+        datetuple = [struct_times.tm_year, struct_times.tm_mon, struct_times.tm_mday]
+        content['hour'] = struct_times.tm_hour
+        content['minute'] = struct_times.tm_min
     else:
-        havecontent=True
-        if not np.isnan(content['start_time']):
-            struct_times=time.localtime(content['start_time'])
-            print(struct_times)
-            datetuple=[struct_times.tm_year,struct_times.tm_mon,struct_times.tm_mday]
-            content['hour']=struct_times.tm_hour
-            content['minute'] = struct_times.tm_min
-        else:
-            datetuple=None
+        datetuple = None
+    values=content_interface(key, content, datetuple)
+    #print(find('id',key[0],df)[0])
+    print(values)
+    df.loc[find('id',key[0],df)[0]]=values
+
+    df.to_csv(file, index=False)
+
+
+def add_content(key,df,file):  #
+    # if content==None:
+    content={'name':'','num':'','hour':'','minute':'','repetition_gap':1,'task_value':''}
+    datetuple = None
+    havecontent=False
+    # else:
+    #     havecontent=True
+    #     if not np.isnan(content['start_time']):
+    #         struct_times=time.localtime(content['start_time'])
+    #         print(struct_times)
+    #         datetuple=[struct_times.tm_year,struct_times.tm_mon,struct_times.tm_mday]
+    #         content['hour']=struct_times.tm_hour
+    #         content['minute'] = struct_times.tm_min
+    #     else:
+    #         datetuple=None
+    values=content_interface(key,content,datetuple)
+    values['father']=key
+    df = pd.concat([df, pd.Series(values).to_frame().T], ignore_index=True)
+    df.to_csv(file, index=False)
+
+
+def content_interface(key,content,datetuple):
     layout = [[sg.Text('名字')], [sg.Input(content['name'],key='name')],
               [sg.Text('重复次数')], [sg.Input(content['num'],key='num')],
               [sg.Text('截止时间')],
@@ -128,48 +148,67 @@ def todo_content(key,todoTree,df,file,content=None):
         event, values = windowadd.read()
         if event in (sg.WIN_CLOSED, 'cancel','Escape:27'):
             windowadd.close()
-            return todoTree
+            return values
         if event in  ('ok','\r', QT_ENTER_KEY1, QT_ENTER_KEY2):
             break
         if event == 'date':
             datetuple = cl.popup_get_date()
-            datetuple = cl.popup_get_date()
             windowadd['date_tuple'].update(str(datetuple))
     windowadd.close()
-    try:
-        values = set_default(key, values, datetuple,content,havecontent)
-    except TypeError:
-        return todoTree
-    if havecontent:
-        df=df.drop([find('id',key[0],df)[0]])
+    values['id']=key
+    values = set_default(key, values, datetuple,content)
+    return values
 
-    df = pd.concat([df, pd.Series(values).to_frame().T], ignore_index=True)
-    df.to_csv(file, index=False)
-    return todoTree
+def cauculate_ddl(start_time):
+    now=datetime.now()
+    #print((now-start_time)//(24*60*60))
+
+    if np.isnan(start_time):
+        return ''
+
+    else:
+        delta1 =  datetime.fromtimestamp(int(start_time))-now
+        if delta1<timedelta(0):
+            return '已经超时'
+        else:
+        #print(type(datetime.fromtimestamp(int(start_time))))
+
+        #if now-start_time>float(time.mktime(datetime(1970,1,1,0,0,0,0).timetuple())):
+            print(delta1)
+            if delta1.days>=1:
+                return str(delta1.days)+'天'
+            else:
+                return str(delta1.min) + '分'+str(delta1.seconds)+'秒'
 
 
-def readcsv(file):
+
+
+
+def readcsv(file): #读文件 ，并生成树状结构
     todoTree = sg.TreeData()
     df = pd.read_csv(file)
-    for i in df.index:
+    df1=df[pd.isna(df['father'])]
+    df2=df[np.logical_not(pd.isna(df['father']))]
+    for i in df1.index:
         key = df.at[i, "id"]
         text = df.at[i, "name"]
-        todoTree.insert('',key,text,[])
-    for i in df.index:
+        ddl=cauculate_ddl(df.at[i,'start_time'])
+        todoTree.insert('',key,text,[ddl])
+    for i in df2.index:
         parent = df.at[i, "father"]
         key = df.at[i, "id"]
         text = df.at[i, "name"]
-        if np.isnan(parent):
-            parent = ""
-        todoTree.tree_dict[todoTree.tree_dict[key].parent].children.remove(todoTree.tree_dict[key])
-        todoTree.tree_dict[key].parent=parent
-        todoTree.tree_dict[parent].children.append(todoTree.tree_dict[key])
+        ddl = cauculate_ddl(df.at[i, 'start_time'])
+        #todoTree.tree_dict[todoTree.tree_dict[key].parent].children.remove(todoTree.tree_dict[key])
+        # todoTree.tree_dict[key].parent=parent
+        # todoTree.tree_dict[parent].children.append(todoTree.tree_dict[key])
+        todoTree.insert(parent, key, text, [ddl])
 
 
 
     layout = [
         [sg.Button('cancel')],
-        [sg.Tree(data=todoTree, headings=['Size', ],
+        [sg.Tree(data=todoTree, headings=['截止日期', ],
                  auto_size_columns=True,
                  select_mode=sg.TABLE_SELECT_MODE_BROWSE,
                  num_rows=20,
@@ -186,14 +225,14 @@ def readcsv(file):
     ]
     return df,todoTree,layout
 
-def delete_in_tree(df,file,values,window):
+def delete_in_tree(df,file,values,window): # 删除树状图里面的内容
     df = tododelete(df, file, values['TREE'])
     df.to_csv(file, index=False)
     df, todoTree, layout = readcsv(file)
     window['TREE'].update(todoTree)
     return df,file,window
 
-def tododelete(df,file,key):
+def tododelete(df,file,key):  # 删除数据里面的内容
     df=df[df.id!=key[0]]
 
     if not df[df.father==key[0]].empty:
@@ -202,7 +241,7 @@ def tododelete(df,file,key):
     df=df[df.father!=key[0]]
     return df
 
-def finished_delete(todo,file,window,values):
+def finished_delete(todo,file,window,values): # 任务结束后删除内容
     # try:
     exist_todo = todo
     df=exist_todo
@@ -231,7 +270,7 @@ def finished_delete(todo,file,window,values):
         print("wrong")
         return False
 
-def pause_task(current_task: Task, command: str):
+def pause_task(current_task: Task, command: str): # 任务暂停
     if current_task.task_name == "":
         sg.Text('当前没有任务')
     else:
@@ -248,7 +287,7 @@ def pause_task(current_task: Task, command: str):
             current_task.task_name = ""
     return current_task
 
-def finished_task(current_task: Task, command: str):
+def finished_task(current_task: Task, command: str): # 任务结束
     if current_task.task_name == "":
         if command == "ok":
             sg.Popup('当前没有任务')
@@ -259,14 +298,14 @@ def finished_task(current_task: Task, command: str):
     return current_task
 
 
-def begin_task(current_task: Task, task_name: str):
+def begin_task(current_task: Task, task_name: str): # 任务开始
     current_task.task_name = task_name
     print(f"开始执行: {current_task.task_name}")
     current_task.start_time = datetime.now()
     return current_task
 
 
-def end_task(current_tasks: Task, reason: str):
+def end_task(current_tasks: Task, reason: str): # 任务结束记录
     current_tasks.end_time = datetime.now()
     layout=[[sg.Text("请输入你完成的内容")],[sg.Input()],[sg.Button('ok')]]
     window=sg.Window("请输入你完成的内容",layout,return_keyboard_events=True)
@@ -302,13 +341,13 @@ def main():
     while True:     # Event Loop
 
         event, values = window.read()
-        print(event,values)
+        element=window.find_element_with_focus()
+        print(event,values,element)
 
         if event in (sg.WIN_CLOSED, 'cancel','Escape:27'):
             break
         if event =='add':
-
-            todo_content(values['TREE'],todoTree,df,file)
+            add_content(values['TREE'],df,file)
             df, todoTree, layout = readcsv(file)
             window['TREE'].update(todoTree)
         if event=='clear':
@@ -319,18 +358,19 @@ def main():
             content=dict(df.loc[find('id',values['TREE'][0],df)[0]])
             content['hour']=''
             content['minute']=''
-            todo_content(values['TREE'], todoTree, df, file,content)
+            revise_content(values['TREE'], df, file,content)
             df, todoTree, layout = readcsv(file)
             window['TREE'].update(todoTree)
         if event in ('start',' '):
-            current_task = begin_task(current_task, df.at[find('id',values['TREE'][0],df)[0], 'name'])
-            start_time,endtime,clockevent,paused=clock.main(current_task.task_name)
-            if clockevent=='-Finished-':
-                current_task = finished_task(current_task, 'q')
-                df,file,window=finished_delete(df,file,window,values)
-            if clockevent in (sg.WIN_CLOSED, 'Exit','-RUN-PAUSE-'):
-                if paused==True:
-                    current_task = pause_task(current_task, 'p')
+            if type(element)==type(window['TREE']):
+                current_task = begin_task(current_task, df.at[find('id',values['TREE'][0],df)[0], 'name'])
+                start_time,endtime,clockevent,paused=clock.main(current_task.task_name)
+                if clockevent=='-Finished-':
+                    current_task = finished_task(current_task, 'q')
+                    df,file,window=finished_delete(df,file,window,values)
+                if clockevent in (sg.WIN_CLOSED, 'Exit','-RUN-PAUSE-'):
+                    if paused==True:
+                        current_task = pause_task(current_task, 'p')
 
         # 下面是键盘映射
 
@@ -347,7 +387,7 @@ def main():
                     content = dict(df.loc[find('id', values['TREE'][0], df)[0]])
                     content['hour'] = ''
                     content['minute'] = ''
-                    todo_content(values['TREE'], todoTree, df, file, content)
+                    revise_content(values['TREE'], df, file, content)
                     df, todoTree, layout = readcsv(file)
                     window['TREE'].update(todoTree)
         if event=='Edit Me':
@@ -355,7 +395,7 @@ def main():
                 content = dict(df.loc[find('id', values['TREE'][0], df)[0]])
                 content['hour'] = ''
                 content['minute'] = ''
-                todo_content(values['TREE'], todoTree, df, file, content)
+                revise_content(values['TREE'], df, file, content)
                 df, todoTree, layout = readcsv(file)
                 window['TREE'].update(todoTree)
         if event=='Delete:46':
@@ -365,7 +405,7 @@ def main():
                 values['TREE']=df.at[0,'id']
                 window['TREE'].update(todoTree)
         if event in ('+'):
-            todo_content(values['TREE'], todoTree, df, file)
+            add_content(values['TREE'], df, file)
             df, todoTree, layout = readcsv(file)
             window['TREE'].update(todoTree)
 
